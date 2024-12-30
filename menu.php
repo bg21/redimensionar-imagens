40ecdbf82a48f5a0e740d28cf64a63c03521a15b
Juliana <juhcosta23@gmail.com>
2024-12-30 17:22:24 -0300

    finalizado

---
+++ menu.php
@@ -0,0 +1,61 @@
+<nav class="bg-dark fixed-top navbar navbar-dark navbar-expand-lg py-2">
+        <div class="align-items-center container"><a class="align-items-center d-flex navbar-brand" href="https://breviares.com/blog/" alt="Breviares - Blog - Conheça as principais tecnologias." title="Breviares - Blog - Conheça as principais tecnologias.">
+
+                <svg width="160" height="44" viewBox="0 0 251.5606737702323 64.8" class="looka-1j8o68f">
+                    <defs id="SvgjsDefs37771"></defs>
+                    <g id="SvgjsG37772" featurekey="symbolFeature-0" transform="matrix(0.7623581416606917,0,0,0.7623581416606917,-17.413228490974454,-1.9055678220102172)" fill="#007bff">
+                        <path xmlns="http://www.w3.org/2000/svg" d="M65.532,53.661c2.473-2.341,2.081-5.645-0.869-7.341L59.1,43.118c-2.95-1.697-4.723-5.797-3.941-9.109l6.699-28.398  c0.781-3.312-0.602-4.108-3.073-1.767L24.467,36.339c-2.472,2.341-2.08,5.644,0.87,7.342l5.562,3.2  c2.949,1.698,4.724,5.798,3.943,9.109l-6.701,28.398c-0.78,3.312,0.602,4.107,3.074,1.767L65.532,53.661z"></path>
+                    </g>
+                    <g id="SvgjsG37773" featurekey="sloganFeature-0" transform="matrix(0.7752173411349809,0,0,0.7752173411349809,46.87202703768367,44.12391329432509)" fill="#ffffff">
+                        <path d="M7.3535 9.766 c2.6367 0 4.8242 1.9531 4.8242 5.2344 s-2.1875 5.2344 -4.8242 5.2344 c-1.3867 0 -2.5977 -0.55664 -3.1641 -1.543 l-0.068359 1.3086 l-2.666 0 l0 -15 l2.8125 0 l0 6.1816 c0.58594 -0.9082 1.7578 -1.416 3.0859 -1.416 z M6.6992 17.9004 c1.4941 0 2.666 -1.1035 2.666 -2.9004 c0 -1.8066 -1.1719 -2.9102 -2.666 -2.9102 c-1.4844 0 -2.6563 1.1523 -2.6563 2.9102 s1.1621 2.9004 2.6563 2.9004 z M14.296896875 20 l0 -15 l2.8418 0 l0 15 l-2.8418 0 z M24.72655 20.19531 c-3.1934 0 -5.498 -1.9434 -5.498 -5.2246 c0 -3.2617 2.2852 -5.2051 5.498 -5.2051 c3.2324 0 5.5078 1.9434 5.5078 5.2051 c0 3.2813 -2.2852 5.2246 -5.5078 5.2246 z M24.69725 17.959 c1.6309 0 2.7441 -1.1914 2.7441 -2.9883 s-1.1133 -2.9883 -2.7441 -2.9883 c-1.5723 0 -2.6758 1.1914 -2.6758 2.9883 s1.1035 2.9883 2.6758 2.9883 z M39.238240625 9.961 l2.6758 0 l0 9.2871 c0 3.9063 -2.1191 5.4883 -5.3223 5.4883 c-2.8809 0 -4.4434 -1.2109 -5.1758 -3.1152 l2.334 -0.99609 c0.56641 1.2988 1.3867 1.9238 2.7344 1.9238 c1.7773 0 2.6074 -1.1133 2.6074 -3.0957 l0 -1.1719 c-0.58594 0.80078 -1.7383 1.3672 -3.0469 1.3672 c-2.4902 0 -4.5801 -1.9629 -4.5801 -4.9609 c0 -3.0078 2.0996 -4.9219 4.5996 -4.9219 c1.4063 0 2.5586 0.625 3.1055 1.5234 z M36.767540625 17.373 c1.4648 0 2.5977 -1.1914 2.5977 -2.6855 c0 -1.5039 -1.1133 -2.6953 -2.5977 -2.6953 c-1.4746 0 -2.5879 1.1426 -2.5879 2.6953 c0 1.5332 1.1328 2.6855 2.5879 2.6855 z"></path>
+                    </g>
+                    <g id="SvgjsG37774" featurekey="nameLeftFeature-0" transform="matrix(2.363073076929337,0,0,2.363073076929337,44.56163194846563,-9.816307390325012)" fill="#ffffff">
+                        <path d="M7.3535 9.766 c2.6367 0 4.8242 1.9531 4.8242 5.2344 s-2.1875 5.2344 -4.8242 5.2344 c-1.3867 0 -2.5977 -0.55664 -3.1641 -1.543 l-0.068359 1.3086 l-2.666 0 l0 -15 l2.8125 0 l0 6.1816 c0.58594 -0.9082 1.7578 -1.416 3.0859 -1.416 z M6.6992 17.9004 c1.4941 0 2.666 -1.1035 2.666 -2.9004 c0 -1.8066 -1.1719 -2.9102 -2.666 -2.9102 c-1.4844 0 -2.6563 1.1523 -2.6563 2.9102 s1.1621 2.9004 2.6563 2.9004 z M16.992196875 11.5527 c0.50781 -0.88867 1.8262 -1.709 3.2813 -1.709 l0 2.5098 c-2.1289 0 -3.1445 0.625 -3.1445 2.7539 l0 4.8926 l-2.832 0 l0 -10.039 l2.6953 0 l0 1.5918 z M31.36675 14.834 l-0.029297 0.67383 l-7.4414 0 c0.11719 1.6699 1.2305 2.4707 2.5684 2.4707 c1.0059 0 1.7871 -0.46875 2.168 -1.3574 l2.5781 0.38086 c-0.66406 2.0313 -2.4707 3.1934 -4.7266 3.1934 c-3.2422 0 -5.3613 -1.9141 -5.3613 -5.2148 s2.1777 -5.2344 5.2734 -5.2344 c2.8418 0 4.9609 1.5723 4.9707 5.0879 z M26.41605 11.7383 c-1.2793 0 -2.168 0.5957 -2.4414 1.9336 l4.6094 0 c-0.14648 -1.2793 -1.0156 -1.9336 -2.168 -1.9336 z M40.03908125 9.961 l2.9785 0 l-4.2773 10.039 l-2.4414 0 l-4.2285 -10.039 l2.959 0 l2.5 6.8652 z"></path>
+                    </g>
+                    <g id="SvgjsG37775" featurekey="nameRightFeature-0" transform="matrix(2.5104602243329266,0,0,2.5104602243329266,149.88702929788556,-10.553301881145845)" fill="#d9d9d9">
+                        <path d="M1.8 5.859999999999999 l0 0.64 l-0.56 0 l0 -0.64 l0.56 0 z M1.8 9.6 l0 10.4 l-0.56 0 l0 -10.4 l0.56 0 z M9.02 9.42 c1.8 0 3.54 0.82 4.5 2.8 l0 -2.62 l0.56 0 l0 10.4 l-0.56 0 l0 -2.56 c-0.96 1.92 -2.7 2.76 -4.5 2.76 c-2.32 0 -5.28 -1.84 -5.28 -5.38 c0 -3.58 2.96 -5.4 5.28 -5.4 z M9.02 9.96 c-2.04 0 -4.7 1.56 -4.7 4.86 c0 3.28 2.66 4.84 4.7 4.84 c2.12 0 4.52 -1.52 4.52 -4.84 c0 -3.34 -2.4 -4.86 -4.52 -4.86 z M21.22 9.44 c0.1 0 0.2 0 0.28 0.02 l0 0.52 c-0.1 -0.02 -0.2 -0.02 -0.3 -0.02 c-2.02 0 -3.7 1.46 -4.08 4.08 l0 5.96 l-0.56 0 l0 -10.4 l0.56 0 l0 3.08 c0.12 -1.54 1.76 -3.24 4.1 -3.24 z M27.620000000000005 9.42 c2.36 0 5.4 1.82 5.16 5.64 l-9.8 0 c0.14 3.1 2.54 4.6 4.84 4.6 c1.54 0 2.82 -0.48 3.64 -1.56 l0.42 0.32 c-0.96 1.28 -2.38 1.78 -4.04 1.78 c-2.72 0 -5.44 -1.82 -5.44 -5.4 c0 -3.56 2.86 -5.38 5.22 -5.38 z M27.620000000000005 9.96 c-1.98 0 -4.48 1.48 -4.64 4.56 l9.24 0 c-0.02 -3.08 -2.62 -4.56 -4.6 -4.56 z M39.56 11.6 c-0.46 -0.94 -1.32 -1.66 -2.6 -1.66 c-1.4 0 -2.28 0.86 -2.28 1.96 c0 0.94 0.48 1.68 1.94 2.2 l0.84 0.3 c1.92 0.66 3.04 1.38 3.04 3.02 c0 1.88 -1.8 2.78 -3.32 2.78 c-1.7 0 -3.14 -0.96 -3.48 -2.66 l0.5 -0.12 c0.38 1.52 1.66 2.24 2.98 2.24 c1.3 0 2.76 -0.7 2.76 -2.18 c0 -1.4 -0.94 -2.02 -2.42 -2.52 l-1.12 -0.4 c-1.64 -0.56 -2.28 -1.56 -2.28 -2.64 c0 -1.4 1.18 -2.5 2.86 -2.5 c1.44 0 2.52 0.8 3.04 1.98 z"></path>
+                    </g>
+                </svg>
+            </a><button class="navbar-toggler" type="button" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" data-bs-target="#navbarNav" data-bs-toggle="collapse"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="mx-auto navbar-nav">
+                    <li class="nav-item"><a class="badge-hover nav-link" href="https://breviares.com/blog/" title="Início"><i class="fas me-1 fa-home"></i> Início</a></li>
+                    <li class="nav-item dropdown"><a class="badge-hover nav-link dropdown-toggle" href="#" title="Cursos" aria-expanded="false" data-bs-toggle="dropdown" id="cursosDropdown" role="button"><i class="fas me-1 fa-chalkboard-teacher"></i> Cursos</a>
+                        <ul class="dropdown-menu" aria-labelledby="cursosDropdown">
+                            <li><a class="dropdown-item" href="https://hotm.art/plano-vitalicio?vem=blog" title="Plano Vitalício" target="__blank">Plano Vitalício</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/pacotefull-stackmaster?vem=blog" title="Pacote Full-Stack" target="__blank">Pacote Full-Stack</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/jornada-do-programador-extraordinario?vem=blog" title="Jornada do Programador Extraordinario" target="__blank">Jornada do Programador Extraordinario</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/curso-de-python-completo?vem=blog" title="Curso de Python Completo" target="__blank">Curso de Python Completo</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/viver-de-freela?vem=blog" title="Curso Viver de Freela" target="__blank">Curso Viver de Freela</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/o-melhor-curso-de-games?vem=blog" title="Curso de Unity e Game Maker" target="__blank">Curso de Unity e Game Maker</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/python-data-science?vem=blog" title="Python para Data Science" target="__blank">Python para Data Science</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/curso-excel-e-power-bi?vem=blog" title="Curso de Excel e PowerBI" target="__blank">Curso de Excel e PowerBI</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/analista-de-dados-completo?vem=blog" title="Analista de Dados com PowerBI" target="__blank">Analista de Dados com PowerBI</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/curso-de-hacker-etico?vem=blog" title="Curso de Hacker Ético" target="__blank">Curso de Hacker Ético</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown"><a class="badge-hover nav-link dropdown-toggle" href="#" title="Ebooks" aria-expanded="false" data-bs-toggle="dropdown" id="ebooksDropdown" role="button"><i class="fas me-1 fa-book"></i> Ebooks</a>
+                        <ul class="dropdown-menu" aria-labelledby="ebooksDropdown">
+                            <li><a class="dropdown-item" href="https://hotm.art/programador-php?vem=blog" title="Programador PHP Relâmpago" target="__blank">Programador PHP Relâmpago</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/ebook-guia-do-html-gratis?vem=blog" title="Guia do HTML - Grátis" target="__blank">Guia do HTML - Grátis</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/ebook-full-stack-master-gratis?vem=blog" title="Guia do Desenvolvedor Full-Stack - Grátis" target="__blank">Guia do Desenvolvedor Full-Stack - Grátis</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/ebook-guia-do-dev-games-gratis?vem=blog" title="Guia do Desenvolvedor de Games - Grátis" target="__blank">Guia do Desenvolvedor de Games - Grátis</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/ebook-gratis-java?vem=blog" title="Guia do Desenvolvedor Java - Grátis" target="__blank">Guia do Desenvolvedor Java - Grátis</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/ebook-viver-negocio-online?vem=blog" title="Viver de Negócio Online - Grátis" target="__blank">Viver de Negócio Online - Grátis</a></li>
+                            <li><a class="dropdown-item" href="https://hotm.art/ebook-gratis-excel?vem=blog" title="Excel para Iniciantes - Grátis" target="__blank">Excel para Iniciantes - Grátis</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a class="badge-hover nav-link highlighted-link" href="https://breviares.com/" title="Breviares - Criação de Sites" target="_blank"><i class="fas me-1 fa-laptop-code"></i> Criação de Sites</a></li>
+                    <li class="nav-item dropdown"><a class="badge-hover nav-link dropdown-toggle" href="#" title="Ferramentas" aria-expanded="false" data-bs-toggle="dropdown" id="ferramentasDropdown" role="button"><i class="fas me-1 fa-book"></i> Ferramentas</a>
+                        <ul class="dropdown-menu" aria-labelledby="ferramentasDropdown">
+                            <li><a class="dropdown-item" href="https://breviares.com/link-whatsapp" title="Gerar Link Whatsapp" target="__blank">Gerar Link Whatsapp</a></li>
+
+                            <li><a class="dropdown-item" href="https://breviares.com/gerador-politicas-privacidade" title="Gerar Políticas de Privacidade" target="__blank">Gerar Políticas de Privacidade</a></li>
+
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
